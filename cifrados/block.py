8e0235bcb974f97244ec8c6f02e00cfd775ee2bf
Cloud Administrator User <cloud-admin@irgt-development-052208.comcel.com.gt>
2024-02-28 15:39:11 -0600

    problema 1 completo

--- cifrados/block.py
+++ cifrados/block.py
@@ -18,14 +18,19 @@ def ecb_encrypt(input_image_path, output_image_path, key):
 
 # ECB Mode Decryption
 def ecb_decrypt(input_image_path, output_image_path, key):
-    image = Image.open(input_image_path).convert('RGB')
-    encrypted_data = image.tobytes()
-    
+    # Asegúrar de que la clave sea de 16 bytes
+    key = bytes.fromhex(key)
+    if len(key) != 16:
+        raise ValueError("La clave debe ser de 16 bytes")
+
+    with open(input_image_path, 'rb') as f:
+        encrypted_data = f.read()
+
     cipher = AES.new(key, AES.MODE_ECB)
     decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
-    
-    with Image.frombytes('RGB', image.size, decrypted_data) as im_ecb:
-        im_ecb.save(output_image_path)
+
+    with open(output_image_path, 'wb') as f:
+        f.write(decrypted_data)
 
 # CBC Mode Encryption
 def cbc_encrypt(input_image_path, output_image_path, key):
@@ -42,17 +47,23 @@ def cbc_encrypt(input_image_path, output_image_path, key):
 
 # CBC Mode Decryption
 def cbc_decrypt(input_image_path, output_image_path, key):
-    image = Image.open(input_image_path)
-    encrypted_data = image.tobytes()
+
+    with open(input_image_path, 'rb') as file:
+        data = file.read()
+
+    iv = data[:AES.block_size]
+    encrypted_data = data[AES.block_size:]
+
+    key = bytes.fromhex(key)
     
-    iv = encrypted_data[:AES.block_size]
-    encrypted_data = encrypted_data[AES.block_size:]
-    
+    if len(key) != 16:
+        raise ValueError("La clave debe ser de 16 bytes")
+
     cipher = AES.new(key, AES.MODE_CBC, iv)
     decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
-    
-    with Image.frombytes('RGB', image.size, decrypted_data) as im_cbc:
-        im_cbc.save(output_image_path)
+
+    with open(output_image_path, 'wb') as file:
+        file.write(decrypted_data)
 
 def main():
 
@@ -81,6 +92,25 @@ def main():
 
     ecb_encrypt(input_image_path, output_ecb_image_path, key)
     cbc_encrypt(input_image_path, output_cbc_image_path, key)
+
+    # Decrypt
+    # increbile
+    with open('./keys/mr-increible.key', 'r') as f:
+        increibleKey = f.read()
+    
+    input_image_path = 'images/mr-increible_encrypted_image.jpeg'
+    output_ecb = 'images/mr-increible_decrypted_image.jpeg'
+
+    ecb_decrypt(input_image_path, output_ecb, increibleKey)
+
+    # ayno
+    with open('./keys/ayno.key', 'r') as f:
+        aynoKey = f.read()
+
+    input_image_path = 'images/ayno_encrypted_image.jpeg'
+    output_cbc = 'images/ayno_decrypted_image.jpeg'
+
+    cbc_decrypt(input_image_path, output_cbc, aynoKey)
 
 if __name__ == "__main__":
     main()
